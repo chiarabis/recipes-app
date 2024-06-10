a3bda90636aa04272a4f213d3ae17821f9d043ed
Chiara <124071052+chiarabis@users.noreply.github.com>
2024-06-10 10:09:16 +0200

    Add files via upload

---
+++ src/pages/MealType.jsx
@@ -0,0 +1,70 @@
+import {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+function MealType() {
+    const apiKey = import.meta.env.VITE_SOME_KEY;
+    const [mealType, setMealType] = useState([])
+    let params = useParams()
+
+    const getMealType = async(name) => {
+        try{
+            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&type=${name}`)
+            if (!data.ok){
+                throw new Error('Errore durante il recupero dei dati');
+            }
+            const recipes = await data.json()
+            setMealType(recipes.results)
+        } catch(error){
+            console.error('Si è verificato un errore durante il recupero dei dati:', error);
+        }     
+    }
+
+    useEffect(()=> {
+        getMealType(params.type, apiKey)
+    }, [params.type])
+
+
+    return (
+    <Grid
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        exit={{opacity: 0}}
+        transition={{duration: 0.5}}
+        >
+        {mealType.map((recipe) => {
+            return (
+                <Card key={recipe.id}>
+                    <Link to={"/recipe/" + recipe.id}>
+                        <img src={recipe.image} alt={recipe.title}/>
+                        <h4>{recipe.title}</h4>
+                    </Link>
+                </Card>
+            )
+        })}
+    </Grid>
+    )
+}
+
+const Grid = styled(motion.div)`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+    grid-gap: 3rem;
+    `;
+const Card = styled.div`
+    img{
+        width: 100%;
+        border-radius: 2rem;
+    }
+    a{
+        text-decoration: none;
+    }
+    h4{
+        text-align: center;
+        padding: 1rem;
+    }
+`;
+
+export default MealType;
